1c7045d91533ee78decf22e9e58f963acdc7f342
Toxolo <111747542+Toxolo@users.noreply.github.com>
2025-06-09 20:48:34 +0200

    Add files via upload

---
+++ Teclat.java
@@ -0,0 +1,19 @@
+package Examen; // Paquet del teclat
+
+import java.util.Scanner; // Importar Scanner
+
+public class Teclat {
+    static Scanner sc = new Scanner(System.in); // Scanner únic
+
+    static public String scString() { return sc.nextLine(); } // Llig línia. Ex: String nom = Teclat.scString();
+
+    static public int scInt() { // Llig enter. Ex: int edat = Teclat.scInt();
+        int resultat;
+        resultat = sc.nextInt(); sc.nextLine(); // Neteja buffer
+        return resultat;
+    }
+
+    static public char scChar() { return sc.nextLine().toUpperCase().charAt(0); } // Llig lletra. Ex: char op = Teclat.scChar();
+
+    static public void nl() { sc.nextLine(); } // Neteja línia. Ex: Teclat.nl();
+}
